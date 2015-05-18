22079c37957fdc8ece9086374d96f56baa56c913
LunaLMM <ljb8901@163.com>
2015-05-18 11:38:50 +0200

    delete some comment in main.cpp for testing the commit push in github---------------JB

--- main.cpp
+++ main.cpp
@@ -6,43 +6,43 @@
 
 int main()
 {
-	//-------------------------------------------test JB  ------------------------------------------
-	//test_beginner();
-	//vanillaSwapComparaisonExemple();
-	//Test_McGeneticSwapLMMPricer();
+
+	//test_beginner();							//test_JB
+	//vanillaSwapComparaisonExemple();			//test_JB		
+	//Test_McGeneticSwapLMMPricer();			//test_JB
 
 
 	test_Integrator1D();
 
 
-	//test_Noise();  //------------------------------------Bon SC
-	//test_HGVolatility();  //--------------------------------------Bug: LNK2019 and 1120 
-	//test_Functional(); //----------------------------------Bon
-	//test_BlackGreek(); //----------------------------------Bon
-	//test_McTerminalLmm(); //-------------------------------Condition not implemented yet
+	//test_Noise();  
+	//test_HGVolatility();  
+	//test_Functional(); 
+	//test_BlackGreek(); 
+	//test_McTerminalLmm(); 
 
-	//test_VanillaFRAPricer();  // TODO: test the drift !=0  case. ---------------------------Bon
-	//test_VanillaCapletPricer(); // bug!	 ----------------------?
-	//test_VanillaSwapPricer();  //-----------------------------bon
-	//test_VanillaSwaptionPricer(); // bug!    ---------------------?
-	//test_VanialSwaptionPricer_MCvsApprox(); //-------------------------------Condition not implemented yet
+	//test_VanillaFRAPricer();  
+	//test_VanillaCapletPricer(); 
+	//test_VanillaSwapPricer();  
+	//test_VanillaSwaptionPricer();    
+	//test_VanialSwaptionPricer_MCvsApprox(); 
 
-	//test_SwaptionMarketDataContainer();		//-----------------------------ne compile pas
-	//test_CalibrationWithATMSwaptionVol();		//-----------------------------ne compile pas
-	//test_CalibrationShiftWithSwaptionVolSkew(); //-----------------------------ne compile pas
+	//test_SwaptionMarketDataContainer();		
+	//test_CalibrationWithATMSwaptionVol();		
+	//test_CalibrationShiftWithSwaptionVolSkew();
 	
-	//test_LmmVirtualCorrelationCalibration();  //--------------------------------debug assertion failed
-	//test_LmmCalibrationConfig(); //----------------------------------------pas de contenu
-	//test_LmmGnuplotPrinterMatrix();  //--------------------------------------pas de contenu
-	//test_Interpolation();   //------------------------------------------------pas de contenu
-	//test_GMatrixMapping();  //------------------------------------------------ne compile pas
-	//test_UpperTriangleVanillaSwaptionQuotes();//------------------------------------------------ne compile pas
-	//test_LmmSwaptionMarketData();//------------------------------------------------ne compile pas
-	//test_LmmABCDFinder();	//-----------------------------------------------------Erreur sur E:\\(chemin pour trouver les donnee input)
-	//test_LmmVirtualCalibration();	//--------------------------------------------------bon
-	//test_LmmCalibrationSensitivity();	// -------------------------------------------mouvais input chemin // bon
-	//test_LmmCalibrationMarketData();	// -------------------------------------------mouvais input chemin  // bon
-	//test_LmmRegularizedCalibrationMarketData();   //--------------------------------sans contenu     // bon
+	//test_LmmVirtualCorrelationCalibration();  
+	//test_LmmCalibrationConfig(); 
+	//test_LmmGnuplotPrinterMatrix();  
+	//test_Interpolation();  
+	//test_GMatrixMapping(); 
+	//test_UpperTriangleVanillaSwaptionQuotes(); 
+	//test_LmmSwaptionMarketData(); 
+	//test_LmmABCDFinder();	
+	//test_LmmVirtualCalibration();	
+	//test_LmmCalibrationSensitivity();	
+	//test_LmmCalibrationMarketData();	
+	//test_LmmRegularizedCalibrationMarketData();   
 
 	getchar();
 }
